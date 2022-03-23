f663cd505dce8a09e691f3c2a3a1c9737c92aca7
Maarten Van Loo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-03-23 12:00:41 +0100

    Initial commit

---
+++ src/main/java/NameServer/NamingServerApp.java
@@ -0,0 +1,18 @@
+package NameServer;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
+
+@SpringBootApplication
+public class NamingServerApp {
+    public static void main(String... args) {
+        SpringApplication app = new SpringApplication(NamingServerApp.class);
+        Properties properties = new Properties();
+        properties.put("server.port", "8083");
+        app.setDefaultProperties(properties);
+        app.run(args);
+    }
+
+}
