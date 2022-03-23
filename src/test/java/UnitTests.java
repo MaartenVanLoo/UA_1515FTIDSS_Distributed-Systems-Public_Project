900c5df192748cbc8500a7a452bad17b7629d06b
Maarten Van Loo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-03-23 17:43:23 +0100

    finished namingserver

---
+++ src/test/java/UnitTests.java
@@ -0,0 +1,35 @@
+import org.junit.*;
+import org.junit.runner.RunWith;
+import Client.*;
+import NameServer.*;
+import Node.*;
+import org.mockito.Mockito;
+import org.junit.Before;
+import org.junit.Test;
+public class UnitTests {
+
+    @Test
+    public void addNodeUnique(){
+        //NameServer test = new NameServer();
+
+    }
+
+    public void addNodeExisting(){
+
+    }
+
+    public void sendFileNameIP(){
+
+    }
+
+    public void sendFileNameLower(){
+
+    }
+
+    public void sendFileNameRemove(){
+
+    }
+
+    public void askTwoPCs(){}
+
+}
