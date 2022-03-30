19d9c838ee01711df7c0e0fcc91bfcfd771b1053
Maarten Van Loo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-03-30 16:08:36 +0200

    Update discovery

--- src/main/java/Node/N2NListener.java
+++ src/main/java/Node/N2NListener.java
@@ -43,11 +43,11 @@ public class N2NListener extends Thread {
         while (this.running) {
             try {
                 byte[] receiveData = new byte[1024]; //make new buffer every time!
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                this.listeningSocket.receive(receivePacket);
-                String data = new String(receivePacket.getData()).trim();
+                DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length);
+                this.listeningSocket.receive(receivedPacket);
+                String data = new String(receivedPacket.getData()).trim();
                 System.out.println("Received: " + data);
-                String sourceIp = receivePacket.getAddress().getHostAddress();
+                String sourceIp = receivedPacket.getAddress().getHostAddress();
                 String response ="{}";
 
                 //what type is the received packet?
@@ -56,69 +56,12 @@ public class N2NListener extends Thread {
                 String type = (String) jsonObject.get("type");
                 DatagramPacket responsePacket = null;
                 if (type.equals("Discovery")) {
-                    //discovery message
                     System.out.println("Received discovery message from " + sourceIp);
-                    String name = (String) jsonObject.get("name");
-                    if (name.equals(this.node.getName())) continue; //no answer!
-                    System.out.println("Name: " + name);
-                    int neighbourId = Hashing.hash(name);
-
-                    if (this.node.getId() == this.node.getPrevNodeId() && this.node.getId() == this.node.getNextNodeId()) {
-                        //this is the first node in the ring
-                        this.node.setNextNodeId(neighbourId);
-                        this.node.setPrevNodeId(neighbourId);
-                        response = "{" +
-                                "\"type\":\"NB-next\"," +
-                                "\"currentId\":\"" + this.node.getId() + "\"," +
-                                "\"nextNodeId\":\"" + this.node.getNextNodeId() + "\"" +
-                                "}";
-                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivePacket.getAddress(), receivePacket.getPort());
-                        this.listeningSocket.send(responsePacket);
-                        response = "{" +
-                                "\"type\":\"NB-prev\"," +
-                                "\"currentId\":\"" + this.node.getId() + "\"," +
-                                "\"prevNodeId\":\"" + this.node.getPrevNodeId() + "\"" +
-                                "}";
-                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivePacket.getAddress(), receivePacket.getPort());
-                        this.listeningSocket.send(responsePacket);
-                    }
-                    //TODO: check reasoning for this
-                    /*else if (neighbourId > this.node.getId() && this.node.getNextNodeId() < this.node.getId()){
-                        //This node has the highest id and the next node has the lowest id => now new "highest" node.
-                    }
-                    else if (neighbourId < this.node.getNextNodeId() && this.node.getId() < this.node.getPrevNodeId()){
-                        //This node has the lowest id and the next node has the highest id => now new "lowest" node.
-                    }*/
-                    else if (neighbourId > this.node.getId() && this.node.getNextNodeId() > neighbourId) {
-                        //new node is to the right
-                        this.node.setNextNodeId(neighbourId);
-                        response = "{" +
-                                "\"type\":\"NB-next\"," +
-                                "\"currentId\":\"" + this.node.getId() + "\"," +
-                                "\"nextNodeId\":\"" + this.node.getNextNodeId() + "\"" +
-                                "}";
-                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivePacket.getAddress(), receivePacket.getPort());
-                        this.listeningSocket.send(responsePacket);
-                    }
-                    else if (neighbourId < this.node.getId() && this.node.getPrevNodeId() < neighbourId) {
-                        //new node is to the left
-                        this.node.setPrevNodeId(neighbourId);
-                        response = "{" +
-                                "\"type\":\"NB-prev\"," +
-                                "\"currentId\":\"" + this.node.getId() + "\"," +
-                                "\"prevNodeId\":\"" + this.node.getPrevNodeId() + "\"" +
-                                "}";
-                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivePacket.getAddress(), receivePacket.getPort());
-                        this.listeningSocket.send(responsePacket);
-                    } else {
-                        System.out.println("Received discovery message from " + sourceIp + " but it is not a neighbour");
-                        continue; //no answer!, never send an empty response!
-                    }
-
+                    discoveryHandler(receivedPacket,jsonObject);
                     //print status update
                     this.node.printStatus();
                 } else if (type.equals("Shutdown")) {
-
+                    shutdownHandler();
                 } else {
                     System.out.println("Unknown message type: " + type);
                 }
@@ -126,5 +69,120 @@ public class N2NListener extends Thread {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void discoveryHandler(DatagramPacket receivedPacket,JSONObject jsonObject) throws IOException {
+        //discovery message
+        String name = (String) jsonObject.get("name");
+        if (name.equals(this.node.getName())) return; //no answer!
+        System.out.println("Name: " + name);
+        int neighbourId = Hashing.hash(name);
+
+        if (this.node.getId() == this.node.getPrevNodeId() && this.node.getId() == this.node.getNextNodeId()) {
+            //this is the first node in the ring
+            updateNextNode(neighbourId, receivedPacket);
+            updatePrevNode(neighbourId, receivedPacket);
+                        /*
+                        this.node.setNextNodeId(neighbourId);
+                        this.node.setPrevNodeId(neighbourId);
+                        response = "{" +
+                                "\"type\":\"NB-next\"," +
+                                "\"currentId\":\"" + this.node.getId() + "\"," +
+                                "\"nextNodeId\":\"" + this.node.getNextNodeId() + "\"" +
+                                "}";
+                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivedPacket.getAddress(), receivedPacket.getPort());
+                        this.listeningSocket.send(responsePacket);
+                        response = "{" +
+                                "\"type\":\"NB-prev\"," +
+                                "\"currentId\":\"" + this.node.getId() + "\"," +
+                                "\"prevNodeId\":\"" + this.node.getPrevNodeId() + "\"" +
+                                "}";
+                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivedPacket.getAddress(), receivedPacket.getPort());
+                        this.listeningSocket.send(responsePacket);
+                        */
+        }
+        //TODO: check reasoning for this
+        else if (neighbourId > this.node.getId() && this.node.getNextNodeId() < this.node.getId()){
+            //This node has the highest id and the next node has the lowest id => now new "highest" node.
+            updateNextNode(neighbourId, receivedPacket);
+                        /*
+                        this.node.setNextNodeId(neighbourId);
+                        response = "{" +
+                                "\"type\":\"NB-next\"," +
+                                "\"currentId\":\"" + this.node.getId() + "\"," +
+                                "\"nextNodeId\":\"" + this.node.getNextNodeId() + "\"" +
+                                "}";
+                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivedPacket.getAddress(), receivedPacket.getPort());
+                        this.listeningSocket.send(responsePacket);
+                        */
+        }
+        else if (neighbourId < this.node.getNextNodeId() && this.node.getId() < this.node.getPrevNodeId()){
+            //This node has the lowest id and the next node has the highest id => now new "lowest" node.
+            //new node is to the left
+            updatePrevNode(neighbourId, receivedPacket);
+                        /* this.node.setPrevNodeId(neighbourId);
+                        response = "{" +
+                                "\"type\":\"NB-prev\"," +
+                                "\"currentId\":\"" + this.node.getId() + "\"," +
+                                "\"prevNodeId\":\"" + this.node.getPrevNodeId() + "\"" +
+                                "}";
+                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivedPacket.getAddress(), receivedPacket.getPort());
+                        this.listeningSocket.send(responsePacket);
+
+                        */
+        }
+        else if (neighbourId > this.node.getId() && this.node.getNextNodeId() > neighbourId) {
+            //new node is to the right
+            updateNextNode(neighbourId, receivedPacket);
+                        /*this.node.setNextNodeId(neighbourId);
+                        response = "{" +
+                                "\"type\":\"NB-next\"," +
+                                "\"currentId\":\"" + this.node.getId() + "\"," +
+                                "\"nextNodeId\":\"" + this.node.getNextNodeId() + "\"" +
+                                "}";
+                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivedPacket.getAddress(), receivedPacket.getPort());
+                        this.listeningSocket.send(responsePacket);
+
+                        */
+        }
+        else if (neighbourId < this.node.getId() && this.node.getPrevNodeId() < neighbourId) {
+            //new node is to the left
+            updatePrevNode(neighbourId, receivedPacket);
+                        /*
+                        this.node.setPrevNodeId(neighbourId);
+                        response = "{" +
+                                "\"type\":\"NB-prev\"," +
+                                "\"currentId\":\"" + this.node.getId() + "\"," +
+                                "\"prevNodeId\":\"" + this.node.getPrevNodeId() + "\"" +
+                                "}";
+                        responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivedPacket.getAddress(), receivedPacket.getPort());
+                        this.listeningSocket.send(responsePacket);
+
+                         */
+        } else {
+            System.out.println("Received discovery message from " + receivedPacket.getAddress().getHostAddress() + " but it is not a neighbour");
+            //no answer!, never send an empty response!
+        }
+    }
+    private void shutdownHandler(){}
+    private void updateNextNode(int neighbourId, DatagramPacket receivedPacket) throws IOException {
+        this.node.setNextNodeId(neighbourId);
+        String response = "{" +
+                "\"type\":\"NB-next\"," +
+                "\"currentId\":\"" + this.node.getId() + "\"," +
+                "\"nextNodeId\":\"" + this.node.getNextNodeId() + "\"" +
+                "}";
+        DatagramPacket responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivedPacket.getAddress(), receivedPacket.getPort());
+        this.listeningSocket.send(responsePacket);
+    }
+    private void updatePrevNode(int neighbourId, DatagramPacket receivedPacket) throws IOException {
+        this.node.setPrevNodeId(neighbourId);
+        String response = "{" +
+                "\"type\":\"NB-prev\"," +
+                "\"currentId\":\"" + this.node.getId() + "\"," +
+                "\"prevNodeId\":\"" + this.node.getPrevNodeId() + "\"" +
+                "}";
+        DatagramPacket responsePacket = new DatagramPacket(response.getBytes(StandardCharsets.UTF_8), response.length(), receivedPacket.getAddress(), receivedPacket.getPort());
+        this.listeningSocket.send(responsePacket);
     }
 }
