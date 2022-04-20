ea67a41e120a9d3c7c31ed3d2c609627a086f6cd
Maarten Van Loo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-04-20 20:03:26 +0200

    hash problem?

--- src/main/java/Utils/Hashing.java
+++ src/main/java/Utils/Hashing.java
@@ -15,9 +15,34 @@ public class Hashing {
         String stringHash = new String(digest);
         SynchronizedPrint.printHex(digest);
         System.out.println();
+        //shrink the byte array to an int using xor
+        short hash = bytesToShort(digest);
+        hash &= 0x7FFF; //remove sign bit
+        return hash;
+        /*
         //System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(stringHash.getBytes()));
         long max = 2147483647;
         long min = -2147483648;
         return (int)(((long)stringHash.hashCode()+max)*(32768.0/(max+Math.abs(min))));
+        */
+    }
+
+    private static int bytesToInt(byte[] bytes) {
+        int hash = 0;
+        for (int i = 0; i < bytes.length/4; i++) {
+            hash ^= bytes[i+0] << 0;
+            hash ^= bytes[i+1] << 8;
+            hash ^= bytes[i+2] << 16;
+            hash ^= bytes[i+3] << 24;
+        }
+        return hash;
+    }
+    private static short bytesToShort(byte[] bytes) {
+        short hash = 0;
+        for (int i = 0; i < bytes.length/2; i++) {
+            hash ^= bytes[i+0] << 0;
+            hash ^= bytes[i+1] << 8;
+        }
+        return hash;
     }
 }
