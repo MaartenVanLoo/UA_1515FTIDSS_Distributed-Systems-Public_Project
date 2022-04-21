a1eba0a7a724f4b6f78e033beb9e668bea695a9c
MaartenVanLoo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-04-22 00:34:04 +0200

    update networkmonitor

--- src/main/NetworkMonitor/utils.js
+++
@@ -1,6 +0,0 @@
-function toggle_options(){
-    document.getElementById("dropdown-options").classList.toggle("show");
-}
-function numberWithSpaces(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-}
