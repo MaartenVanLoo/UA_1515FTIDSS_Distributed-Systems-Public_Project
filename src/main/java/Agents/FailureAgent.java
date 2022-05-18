582acb3424afe2fb392b97180003983bc31104a5
Leander <leander.vdheuvel@gmail.com>
2022-05-18 11:31:54 +0200

    Changes

--- src/main/java/Agents/FailureAgent.java
+++ src/main/java/Agents/FailureAgent.java
@@ -1,8 +1,46 @@
 package Agents;
 
+import Node.Node;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import NameServer.*;
 
-public class FailureAgent extends Agent{
+import java.io.Serializable;
+
+public class FailureAgent implements Runnable, Serializable {
+
+    private Node node;
+
+    FailureAgent(Node node, Node thisNode) {
+        this.node = thisNode;
+        Node failingNode = node;
+    }
+
+    //get failing node from Nameserver via REST
+    public void setup() {
+
+    }
+    private class behaviour extends Behaviour {
+        @Override
+        public void action() {
+
+        }
+
+        @Override
+        public boolean done() {
+            return false;
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            //get fileList of current node
 
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
