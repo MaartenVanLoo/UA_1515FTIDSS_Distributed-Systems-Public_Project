6b649fecf9172012e1f26cb8fe92c04aa27600fa
Maarten Van Loo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-05-25 04:39:18 +0200

    update

--- src/main/java/NameServer/NameServerController.java
+++ src/main/java/NameServer/NameServerController.java
@@ -380,10 +380,11 @@ public class NameServerController {
                 if (success) {
                     //notify the previous node that a new node has been created
                     this.nameServerController.nameServer.getIpMapLock().readLock().lock();
+                    String ip = this.nameServerController.nameServer.getIpMapping().get(Id);
                     String previousIp = this.nameServerController.nameServer.getPrevNodeIP(Id);
                     JSONObject json = new JSONObject();
                     json.put("id", Id);
-                    json.put("ip", previousIp);
+                    json.put("ip", ip);
                     System.out.println(previousIp+":8081/files");
                     try {
                         System.out.println("Status:"
